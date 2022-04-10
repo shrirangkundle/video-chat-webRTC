b63847bfc2bf041df3214eb881b04633c241b0b3
Shrirang <shrirangkundle10@gmail.com>
2022-04-10 16:28:11 +0530

    3 final submission

--- chat-interface/src/App.js
+++ chat-interface/src/App.js
@@ -1,17 +1,49 @@
 import React from "react";
 import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import VideoPlayer from "./components/VideoPlayer";
+import Options from "./components/Options";
+import Notifications from "./components/Notifications";
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <>
-      <AppBar position="static" color="inherit">
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography varient="h2" align="center">
-          Video chat
+          Video chat demo
         </Typography>
-        {/* insert video player */}
-        {/* options-notification */}
       </AppBar>
-    </>
+      <VideoPlayer />
+      <Options>
+        <Notifications></Notifications>
+      </Options>
+    </div>
   );
 };
 
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
+
 const io = require("socket.io")(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("server is running");
+  res.send("Running");
 });
+
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
 
@@ -27,6 +33,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`server listening on port : ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
